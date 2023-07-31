89f9279977511d5d974ee2f0324b6aed3b18317b
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2023-07-31 12:08:43 +0545

    2nd

--- index.php
+++ index.php
@@ -9,14 +9,8 @@ include("config.php");
 <html lang="en">
 
 <head>
-<!-- FOR MORE PROJECTS visit: codeastro.com -->
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Meta Tags -->
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 <link rel="shortcut icon" href="images/favicon.ico">
 
 <!--	Fonts
@@ -42,15 +36,6 @@ include("config.php");
 </head>
 <body>
 
-<!--	Page Loader  -->
-<!--<div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
-	<div class="d-flex justify-content-center y-middle position-relative">
-	  <div class="spinner-border" role="status">
-		<span class="sr-only">Loading...</span>
-	  </div>
-	</div>
-</div>  -->
-<!--	Page Loader  -->
 
 <div id="page-wrapper">
     <div class="row"> 
